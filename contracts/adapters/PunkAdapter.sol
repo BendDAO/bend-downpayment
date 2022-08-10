1120e39059e50e8b70a9a8aa628ee9ec4d08219f
akashiceth <akashiceth@gmail.com>
2022-08-10 15:32:11 +0800

    fix: Fix punk adapter init weth

--- contracts/adapters/PunkAdapter.sol
+++ contracts/adapters/PunkAdapter.sol
@@ -34,7 +34,7 @@ contract PunkAdapter is BaseAdapter {
         wpunkProxy = wrappedPunks.proxyInfo(address(this));
     }
 
-    function initWETH() external {
+    function initWETH() external reinitializer(2) {
         __BaseAdapter_init(NAME, VERSION, address(downpayment));
     }
 
